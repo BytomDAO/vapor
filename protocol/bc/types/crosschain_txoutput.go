7ef11431395f5ed40404187fd01655ed10cbcf9f
HAOYUatHZ <haoyu@protonmail.com>
2019-05-10 13:50:04 +0800

    feat: add CrossChainTxOutput

---
+++ protocol/bc/types/crosschain_txoutput.go
@@ -0,0 +1,31 @@
+package types
+
+import (
+	"github.com/vapor/protocol/bc"
+)
+
+// CrossChainTxOutput satisfies the TypedOutput interface and represents a intra-chain transaction.
+type CrossChainTxOutput struct {
+	// TODO:
+	OutputCommitment
+	// Unconsumed suffixes of the commitment and witness extensible strings.
+	// TODO:
+	CommitmentSuffix []byte
+}
+
+// CrossChainTxOutput create a new output struct
+func NewCrossChainTxOutput(assetID bc.AssetID, amount uint64, controlProgram []byte) *TxOutput {
+	return &TxOutput{
+		AssetVersion: 1,
+		OutputCommitment: OutputCommitment{
+			AssetAmount: bc.AssetAmount{
+				AssetId: &assetID,
+				Amount:  amount,
+			},
+			VMVersion:      1,
+			ControlProgram: controlProgram,
+		},
+	}
+}
+
+func (it *CrossChainTxOutput) TypedOutput() uint8 { return IntraChainOutputType }
