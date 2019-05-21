cc0bcc7e1e4fc53e669ac46ba3fe919b5c6a79c9
wz <mars@bytom.io>
2019-05-21 19:32:35 +0800

    unvotetx action (#80)
    * add votetx output action
    
    * add unvotetx action
    
    * add voteoutput change
    
    * add unit test

--- api/transact.go
+++ api/transact.go
@@ -30,6 +30,7 @@ func (a *API) actionDecoder(action string) (func([]byte) (txbuilder.Action, erro
 		"vote_output":                  txbuilder.DecodeVoteOutputAction,
 		"spend_account":                a.wallet.AccountMgr.DecodeSpendAction,
 		"spend_account_unspent_output": a.wallet.AccountMgr.DecodeSpendUTXOAction,
+		"unvote":                       a.wallet.AccountMgr.DecodeUnvoteAction,
 	}
 	decoder, ok := decoders[action]
 	return decoder, ok
