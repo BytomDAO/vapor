b7f09746eaf64a44f748d1f593c160ac91e4f282
mars <mars@bytom.io>
2019-05-21 14:59:51 +0800

    add votetx output action

--- api/transact.go
+++ api/transact.go
@@ -27,6 +27,7 @@ func (a *API) actionDecoder(action string) (func([]byte) (txbuilder.Action, erro
 		"control_program":              txbuilder.DecodeControlProgramAction,
 		"retire":                       txbuilder.DecodeRetireAction,
 		"cross_chain_out":              txbuilder.DecodeCrossOutAction,
+		"vote_output":                  txbuilder.DecodeVoteOutputAction,
 		"spend_account":                a.wallet.AccountMgr.DecodeSpendAction,
 		"spend_account_unspent_output": a.wallet.AccountMgr.DecodeSpendUTXOAction,
 	}
