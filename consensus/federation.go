192531c2ac2dd7ef8108ed25a86cf3f2171b7acb
HAOYUatHZ <haoyu@protonmail.com>
2019-05-23 04:05:42 +0800

    init

---
+++ consensus/federation.go
@@ -0,0 +1,14 @@
+package consensus
+
+import (
+	// "encoding/binary"
+	// "strings"
+
+	// "github.com/vapor/protocol/bc"
+	"github.com/vapor/crypto/ed25519/chainkd"
+)
+
+const (
+	FedXPubs = ""
+	Quorum   = 1
+)
