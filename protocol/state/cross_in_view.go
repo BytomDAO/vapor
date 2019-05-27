8c78a0ebc2e74ee32a5c11f1979a3b6c1ed36684
HAOYUatHZ <haoyu@protonmail.com>
2019-05-27 16:40:44 +0800

    add NewCrossInViewpoint

--- protocol/state/cross_in_view.go
+++ protocol/state/cross_in_view.go
@@ -1,5 +1,15 @@
 package state
 
+import (
+	"github.com/vapor/protocol/bc"
+)
+
 type CrossInViewpoint struct {
 	Entries map[bc.Hash]bool
+}
+
+func NewCrossInViewpoint() *CrossInViewpoint {
+	return &CrossInViewpoint{
+		Entries: make(map[bc.Hash]bool),
+	}
 }
