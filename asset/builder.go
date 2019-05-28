17cf7ff58658df941dfa9dea6cebbcc70cd13872
HAOYUatHZ <haoyu@protonmail.com>
2019-05-28 16:52:12 +0800

    fix https://github.com/Bytom/vapor/pull/91#discussion_r287974969

--- asset/builder.go
+++ asset/builder.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/vapor/blockchain/txbuilder"
 	"github.com/vapor/consensus/federation"
+	chainjson "github.com/vapor/encoding/json"
 	"github.com/vapor/protocol/bc"
 	"github.com/vapor/protocol/bc/types"
 )
@@ -20,9 +21,9 @@ func (r *Registry) DecodeCrossInAction(data []byte) (txbuilder.Action, error) {
 type crossInAction struct {
 	reg *Registry
 	bc.AssetAmount
-	SourceID        bc.Hash                `json:"source_id"`
-	SourcePos       uint64                 `json:"source_pos"`
-	AssetDefinition map[string]interface{} `json:"asset_definition"`
+	SourceID          bc.Hash            `json:"source_id"`
+	SourcePos         uint64             `json:"source_pos"`
+	RawDefinitionByte chainjson.HexBytes `json:"raw_definition_byte"`
 }
 
 func (a *crossInAction) Build(ctx context.Context, builder *txbuilder.TemplateBuilder) error {
@@ -40,13 +41,8 @@ func (a *crossInAction) Build(ctx context.Context, builder *txbuilder.TemplateBu
 		return txbuilder.MissingFieldsError(missing...)
 	}
 
-	rawDefinitionByte, err := serializeAssetDef(a.AssetDefinition)
-	if err != nil {
-		return ErrSerializing
-	}
-
 	// arguments will be set when materializeWitnesses
-	txin := types.NewCrossChainInput(nil, a.SourceID, *a.AssetId, a.Amount, a.SourcePos, nil, rawDefinitionByte)
+	txin := types.NewCrossChainInput(nil, a.SourceID, *a.AssetId, a.Amount, a.SourcePos, nil, a.RawDefinitionByte)
 	tplIn := &txbuilder.SigningInstruction{}
 	fed := federation.GetFederation()
 	tplIn.AddRawWitnessKeys(fed.XPubs, nil, fed.Quorum)
