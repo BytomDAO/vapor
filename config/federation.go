f554858ea74892477ed64c245559e4f85fb5febc
mars <mars@bytom.io>
2019-05-29 10:53:11 +0800

    delete code

--- config/federation.go
+++ config/federation.go
@@ -3,16 +3,8 @@ package config
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
-
-	"github.com/vapor/crypto/ed25519"
-	chainjson "github.com/vapor/encoding/json"
-	"github.com/vapor/equity/compiler"
-	equityutil "github.com/vapor/equity/equity/util"
-	"github.com/vapor/errors"
 )
 
 func ExportFederationFile(fedFile string, config *Config) error {
@@ -35,99 +27,4 @@ func LoadFederationFile(fedFile string, config *Config) error {
 	defer file.Close()
 
 	return json.NewDecoder(file).Decode(config.Federation)
-}
-
-var lockWithKeysFmt = `
-contract LockWithKeys(%s: PublicKey) locks amount of asset {
-	clause unlockWith2Sigs(%s: Signature) {
-		verify checkTxMultiSig([%s],[%s])
-		unlock amount of asset
-	}
-}
-`
-
-func generateFederationContract(pubkeys []ed25519.PublicKey, quorum int) (string, error) {
-	num := len(pubkeys)
-	if num == 0 {
-		return "", errors.New("federation's XPubs is empty")
-	}
-
-	if quorum < len(pubkeys)/2+1 || quorum > num {
-		return "", errors.New("The quorum with multiple contracts in the federation is incorrect")
-	}
-
-	params := ""
-	unlockParams := ""
-	checkPubkeysParams := ""
-	checkSigsParams := ""
-
-	for index := 0; index < num; index++ {
-		param := fmt.Sprintf("pubkey%d", index+1)
-		params += param
-		checkPubkeysParams += param
-		if (index + 1) < num {
-			params += ","
-			checkPubkeysParams += ","
-		}
-	}
-
-	for index := 0; index < quorum; index++ {
-		param := fmt.Sprintf("sig%d", index+1)
-		unlockParams += param
-		checkSigsParams += param
-		if index+1 < quorum {
-			unlockParams += ","
-			checkSigsParams += ","
-		}
-	}
-
-	lockWithKeys := fmt.Sprintf(lockWithKeysFmt, params, unlockParams, checkPubkeysParams, checkSigsParams)
-	return lockWithKeys, nil
-}
-
-func GetFederationContractPrograms(pubkeys []ed25519.PublicKey, quorum int) ([]byte, error) {
-	lockWithKeys, err := generateFederationContract(pubkeys, quorum)
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed generate Federation Contract")
-	}
-
-	r := strings.NewReader(lockWithKeys)
-	compiled, err := compiler.Compile(r)
-	if err != nil {
-		return nil, errors.Wrap(err, "Compile contract failed")
-	}
-
-	contract := compiled[len(compiled)-1]
-
-	if len(pubkeys) < len(contract.Params) {
-		return nil, errors.New("Compile contract failed")
-	}
-
-	contractArgs, err := convertArguments(contract, pubkeys)
-	if err != nil {
-		return nil, errors.Wrap(err, "Convert arguments into contract parameters error")
-	}
-
-	instantProg, err := equityutil.InstantiateContract(contract, contractArgs)
-	if err != nil {
-		return nil, errors.Wrap(err, "Instantiate contract error")
-	}
-
-	return instantProg, nil
-}
-
-func convertArguments(contract *compiler.Contract, args []ed25519.PublicKey) ([]compiler.ContractArg, error) {
-	var contractArgs []compiler.ContractArg
-	for i, p := range contract.Params {
-		var argument compiler.ContractArg
-		switch p.Type {
-		case "PublicKey":
-			argument.S = (*chainjson.HexBytes)(&args[i])
-		default:
-			return nil, errors.New("Contract parameter type error")
-		}
-		contractArgs = append(contractArgs, argument)
-	}
-
-	return contractArgs, nil
 }
--- config/federation_test.go
+++ config/federation_test.go
@@ -1,12 +1,10 @@
 package config
 
 import (
-	"encoding/hex"
 	"io/ioutil"
 	"os"
 	"testing"
 
-	"github.com/vapor/crypto/ed25519"
 	"github.com/vapor/testutil"
 )
 
@@ -35,40 +33,5 @@ func TestFederation(t *testing.T) {
 
 	if !testutil.DeepEqual(config.Federation, loadConfig.Federation) {
 		t.Fatalf("export: %v, load: %v", config.Federation, loadConfig.Federation)
-	}
-}
-
-func TestFederationMulContract(t *testing.T) {
-	wantContract := `
-contract LockWithKeys(pubkey1,pubkey2,pubkey3,pubkey4,pubkey5,pubkey6: PublicKey) locks amount of asset {
-	clause unlockWith2Sigs(sig1,sig2,sig3,sig4: Signature) {
-		verify checkTxMultiSig([pubkey1,pubkey2,pubkey3,pubkey4,pubkey5,pubkey6],[sig1,sig2,sig3,sig4])
-		unlock amount of asset
-	}
-}
-`
-	wantProg := "206798460919e8dc7095ee8b9f9d65033ef3da8c2334813149da5a1e52e9c6da0720d72fb92fa13bf3e0deb39de3a47c8d6eef5584719f7877c82a4c009f78fddf9220983705ae71949c1a5d0fcf953658dd9ecc549f02c63e197b4d087ae31148097e20b58170b51ca61604028ba1cb412377dfc2bc6567c0afc84c83aae1c0c297d02220585e20143db413e45fbc82f03cb61f177e9916ef1df0012daa8cbf6dbb1025ce207f23aae65ee4307c38d342699e328f21834488e18191ebd66823d220b5a58303741b567a577a587a597a546bae5a7a5a7a5a7a5a7a5a7a5a7a566c7cad00c0"
-	config := DefaultConfig()
-	pubKeys := []ed25519.PublicKey{}
-	for _, xpub := range config.Federation.Xpubs {
-		pubKeys = append(pubKeys, xpub.PublicKey())
-	}
-
-	fedContract, err := generateFederationContract(pubKeys, config.Federation.Quorum)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if wantContract != fedContract {
-		t.Fatalf("fedContract: %s, wantContract:%s", fedContract, wantContract)
-	}
-
-	prog, err := GetFederationContractPrograms(pubKeys, config.Federation.Quorum)
-	if err != nil {
-		t.Fatal(err)
-	}
-	got := hex.EncodeToString(prog)
-	if got != wantProg {
-		t.Fatalf("got: %s, want:%s", got, wantProg)
 	}
 }
