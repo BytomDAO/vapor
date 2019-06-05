4319225f389c6aa1ea12a32e9a2bf714722c4cea
HAOYUatHZ <haoyu@protonmail.com>
2019-06-05 13:25:19 +0800

    feat: add orm

--- federation/database/orm/warder.go
+++ federation/database/orm/warder.go
@@ -1,3 +1,12 @@
 package orm
 
-type warder struct{}
+import (
+	"github.com/vapor/federation/types"
+)
+
+type warder struct {
+	ID        uint64
+	Pubkey    string
+	CreatedAt types.Timestamp
+	UpdatedAt types.Timestamp
+}
