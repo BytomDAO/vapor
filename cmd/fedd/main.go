9d1a0f546f05e26f9596be0d9885f67dd034ba31
HAOYUatHZ <haoyu@protonmail.com>
2019-06-05 15:13:22 +0800

    feat: init db

--- cmd/fedd/main.go
+++ cmd/fedd/main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/vapor/federation/config"
+	"github.com/vapor/federation/database"
+	// "github.com/blockcenter/synchron"
+)
+
 func main() {
+	cfg := config.NewConfig()
+	_, err := database.NewMySQLDB(cfg.MySQLConfig)
+	if err != nil {
+		log.WithField("err", err).Panic("initialize mysql db error")
+	}
+
+	// keep the main func running in case of terminating goroutines
+	var wg sync.WaitGroup
+	wg.Add(1)
+	wg.Wait()
 }
