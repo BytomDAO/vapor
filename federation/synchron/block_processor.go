9acec70db2bdbbd643dad9891b1b47970db290fb
HAOYUatHZ <haoyu@protonmail.com>
2019-06-06 14:31:31 +0800

    add addIssueAssets

--- federation/synchron/block_processor.go
+++ federation/synchron/block_processor.go
@@ -1,7 +1,7 @@
 package synchron
 
 import (
-	// "encoding/hex"
+	"encoding/hex"
 	// "encoding/json"
 	// "fmt"
 	// "math/big"
@@ -17,52 +17,40 @@ import (
 	// log "github.com/sirupsen/logrus"
 	// "github.com/blockcenter/coin/btm"
 	// "github.com/blockcenter/config"
-	// "github.com/blockcenter/database/orm"
 	// "github.com/blockcenter/types"
+	"github.com/vapor/federation/database/orm"
 )
 
 func addIssueAssets(db *gorm.DB, txs []*btmTypes.Tx) error {
-	/*
-		var assets []*orm.Asset
-		assetMap := make(map[string]bool)
+	var assets []*orm.Asset
+	assetMap := make(map[string]bool)
 
-		type assetDefinition struct {
-			Decimals uint64 `json:"decimals"`
-		}
-
-		for _, tx := range txs {
-			for _, input := range tx.Inputs {
-				switch inp := input.TypedInput.(type) {
-				case *btmTypes.IssuanceInput:
-					assetID := inp.AssetID()
-					if _, ok := assetMap[assetID.String()]; ok {
-						continue
-					}
-					assetMap[assetID.String()] = true
-
-					asset := &orm.Asset{}
-					definition := &assetDefinition{}
-					if err := json.Unmarshal(inp.AssetDefinition, definition); err != nil {
-						log.WithFields(log.Fields{
-							"err":             err,
-							"AssetDefinition": inp.AssetDefinition,
-						}).Error("json unmarshal AssetDefinition")
-					}
-
-					asset.CoinID = coinID
-					asset.Decimals = definition.Decimals
-					asset.Asset = assetID.String()
-					asset.Definition = string(inp.AssetDefinition)
-					assets = append(assets, asset)
+	for _, tx := range txs {
+		for _, input := range tx.Inputs {
+			switch inp := input.TypedInput.(type) {
+			case *btmTypes.IssuanceInput:
+				assetID := inp.AssetID()
+				if _, ok := assetMap[assetID.String()]; ok {
+					continue
 				}
-			}
-		}
+				assetMap[assetID.String()] = true
 
-		for _, asset := range assets {
-			if err := db.Where(&orm.Asset{CoinID: asset.CoinID, Asset: asset.Asset}).FirstOrCreate(asset).Error; err != nil {
-				return err
+				asset := &orm.Asset{
+					AssetID:           assetID.String(),
+					IssuanceProgram:   hex.EncodeToString(inp.IssuanceProgram),
+					VMVersion:         inp.VMVersion,
+					RawDefinitionByte: hex.EncodeToString(inp.AssetDefinition),
+				}
+				assets = append(assets, asset)
 			}
 		}
-	*/
+	}
+
+	for _, asset := range assets {
+		if err := db.Where(&orm.Asset{AssetID: asset.AssetID}).FirstOrCreate(asset).Error; err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
