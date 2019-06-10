8fb5f907e26d20a19ad9edc40e7cd8ef72271b8d
HAOYUatHZ <haoyu@protonmail.com>
2019-06-10 14:39:34 +0800

    add config

---
+++ federation/database/db.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+
+	"github.com/vapor/errors"
+	"github.com/vapor/federation/config"
+)
+
+func NewMySQLDB(cfg config.MySQLConfig) (*gorm.DB, error) {
+	dsnTemplate := "%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=true&loc=Local"
+	dsn := fmt.Sprintf(dsnTemplate, cfg.Connection.Username, cfg.Connection.Password, cfg.Connection.Host, cfg.Connection.Port, cfg.Connection.DbName)
+	db, err := gorm.Open("mysql", dsn)
+	if err != nil {
+		return nil, errors.Wrap(err, "open db cluster")
+	}
+
+	db.LogMode(cfg.LogMode)
+	if err = db.DB().Ping(); err != nil {
+		return nil, errors.Wrap(err, "ping db")
+	}
+
+	return db, nil
+}
---
+++ federation/synchron/sidechain_keeper.go
@@ -0,0 +1,26 @@
+package synchron
+
+import (
+	"github.com/jinzhu/gorm"
+
+	"github.com/vapor/federation/config"
+	"github.com/vapor/federation/service"
+)
+
+type sidechainKeeper struct {
+	cfg       *config.Chain
+	db        *gorm.DB
+	node      *service.Node
+	chainName string
+}
+
+func NewSidechainKeeper(db *gorm.DB, chainCfg *config.Chain) *sidechainKeeper {
+	return &sidechainKeeper{
+		cfg:       chainCfg,
+		db:        db,
+		node:      service.NewNode(chainCfg.Upstream),
+		chainName: chainCfg.Name,
+	}
+}
+
+func (m *sidechainKeeper) Run() {}
