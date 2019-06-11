92879567cc7b1e06c1fa89f80fc0a485f5e039d4
HAOYUatHZ <haoyu@protonmail.com>
2019-06-11 10:53:35 +0800

    implement add get remove assetCache

--- federation/database/cache.go
+++ federation/database/cache.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"github.com/golang/groupcache/lru"
+
+	"github.com/vapor/federation/database/orm"
 )
 
 const maxAssetCached = 1024
@@ -14,11 +16,18 @@ func NewAssetCache() *AssetCache {
 	return &AssetCache{lruCache: lru.New(maxAssetCached)}
 }
 
-func (a *AssetCache) Add() {
+func (a *AssetCache) Add(assetID string, asset *orm.Asset) {
+	a.lruCache.Add(assetID, asset)
 }
 
-func (a *AssetCache) Get() {
+func (a *AssetCache) Get(assetID string) *orm.Asset {
+	if v, ok := a.lruCache.Get(assetID); ok {
+		return v.(*orm.Asset)
+	}
+
+	return nil
 }
 
-func (a *AssetCache) Remove() {
+func (a *AssetCache) Remove(assetID string) {
+	a.lruCache.Remove(assetID)
 }
