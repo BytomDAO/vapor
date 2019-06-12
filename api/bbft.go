425213ac89ff0bcd2e3514c1b02bb66f433007e1
shenao78 <shenao.78@163.com>
2019-06-12 16:17:01 +0800

    add sort for get vote result api

--- api/bbft.go
+++ api/bbft.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"sort"
+
 	chainjson "github.com/vapor/encoding/json"
 )
 
@@ -9,18 +11,23 @@ type voteInfo struct {
 	VoteNum     uint64 `json:"vote_num"`
 }
 
+type voteInfoSlice []*voteInfo
+func (v voteInfoSlice) Len() int { return len(v) }
+func (v voteInfoSlice) Less(i, j int) bool { return v[i].VoteNum > v[j].VoteNum }
+func (v voteInfoSlice) Swap(i, j int) { v[i], v[j] = v[j], v[i] }
+
 func (a *API) getVoteResult(req struct {
 	BlockHash   chainjson.HexBytes `json:"block_hash"`
 	BlockHeight uint64             `json:"block_height"`
 }) Response {
 	blockHash := hexBytesToHash(req.BlockHash)
 	if len(req.BlockHash) != 32 {
-		blockNode, err := a.chain.GetHeaderByHeight(req.BlockHeight)
+		blockHeader, err := a.chain.GetHeaderByHeight(req.BlockHeight)
 		if err != nil {
 			return NewErrorResponse(err)
 		}
 
-		blockHash = blockNode.Hash()
+		blockHash = blockHeader.Hash()
 	}
 
 	voteResult, err := a.chain.GetVoteResultByHash(&blockHash)
@@ -35,5 +42,6 @@ func (a *API) getVoteResult(req struct {
 			VoteNum:     voteNum,
 		})
 	}
+	sort.Sort(voteInfoSlice(voteInfos))
 	return NewSuccessResponse(voteInfos)
 }
