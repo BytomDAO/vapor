f47413c46121216dae3b1a905fbe53ae559adee2
Chengcheng Zhang <943420582@qq.com>
2019-06-13 21:53:32 +0800

    add block witness update function (#176)
    * add block witness update function
    
    * add Delete and test function
    
    * update
    
    * update
    
    * update
    
    * update

--- protocol/bc/types/block_witness.go
+++ protocol/bc/types/block_witness.go
@@ -20,3 +20,25 @@ func (bw *BlockWitness) readFrom(r *blockchain.Reader) (err error) {
 	bw.Witness, err = blockchain.ReadVarstrList(r)
 	return err
 }
+
+func (bw *BlockWitness) Set(index uint64, data []byte) {
+	if uint64(len(bw.Witness)) <= index {
+		newWitness := make([][]byte, index+1, index+1)
+		copy(newWitness, bw.Witness)
+		bw.Witness = newWitness
+	}
+	bw.Witness[index] = data
+}
+
+func (bw *BlockWitness) Delete(index uint64) {
+	if uint64(len(bw.Witness)) > index {
+		bw.Witness[index] = nil
+	}
+}
+
+func (bw *BlockWitness) Get(index uint64) []byte {
+	if uint64(len(bw.Witness)) > index {
+		return bw.Witness[index]
+	}
+	return nil
+}
