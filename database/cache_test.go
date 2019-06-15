57ab0240feb02d04825644ed30d3128ed100539a
Chengcheng Zhang <943420582@qq.com>
2019-06-15 18:06:02 +0800

    add TestVoteResultCache

--- database/cache_test.go
+++ database/cache_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/vapor/protocol/bc"
 	"github.com/vapor/protocol/bc/types"
+	"github.com/vapor/protocol/state"
 )
 
 func TestBlockCache(t *testing.T) {
@@ -44,6 +45,42 @@ func TestBlockCache(t *testing.T) {
 		hash := block.Hash()
 		if b, _ := cache.get(&hash); b == nil {
 			t.Fatalf("can't find new block")
+		}
+	}
+}
+
+func TestVoteResultCache(t *testing.T) {
+	newVoteResult := func(seq uint64) *state.VoteResult {
+		return &state.VoteResult{
+			Seq: seq,
+		}
+	}
+	voteResults := make(map[uint64]*state.VoteResult)
+	for i := 0; i < maxCachedVoteResults+10; i++ {
+		voteResult := newVoteResult(uint64(i))
+		voteResults[voteResult.Seq] = voteResult
+	}
+
+	cache := newVoteResultCache(func(seq uint64) (*state.VoteResult, error) {
+		return voteResults[seq], nil
+	})
+
+	for i := 0; i < maxCachedVoteResults+10; i++ {
+		voteResult := newVoteResult(uint64(i))
+		cache.lookup(voteResult.Seq)
+	}
+
+	for i := 0; i < 10; i++ {
+		voteResult := newVoteResult(uint64(i))
+		if v, _ := cache.get(voteResult.Seq); v != nil {
+			t.Fatalf("find old vote result")
+		}
+	}
+
+	for i := 10; i < maxCachedVoteResults+10; i++ {
+		voteResult := newVoteResult(uint64(i))
+		if v, _ := cache.get(voteResult.Seq); v == nil {
+			t.Fatalf("can't find new vote result")
 		}
 	}
 }
