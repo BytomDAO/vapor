45912e17e59779c41ac45a48b3933a606d10ed45
HAOYUatHZ <haoyu@protonmail.com>
2019-06-16 09:28:12 +0800

    find

--- federation/warder.go
+++ federation/warder.go
@@ -101,7 +101,7 @@ func (w *warder) collectUnsubmittedTx() {
 	ticker := time.NewTicker(w.colletInterval)
 	for ; true; <-ticker.C {
 		txs := []*orm.CrossTransaction{}
-		if err := w.db.Preload("Reqs").Where(&orm.CrossTransaction{Status: common.CrossTxPendingStatus}).Find(&txs).Error; err == gorm.ErrRecordNotFound {
+		if err := w.db.Preload("Chain").Preload("Reqs").Where(&orm.CrossTransaction{Status: common.CrossTxPendingStatus}).Find(&txs).Error; err == gorm.ErrRecordNotFound {
 			continue
 		} else if err != nil {
 			log.Warnln("collectUnsubmittedTx", err)
