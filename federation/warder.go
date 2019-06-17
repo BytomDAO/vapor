721dbd6b7b149c23d6cf18e963f5438fe5e45c18
HAOYUatHZ <haoyu@protonmail.com>
2019-06-17 10:15:31 +0800

    dododo

--- federation/warder.go
+++ federation/warder.go
@@ -19,7 +19,7 @@ import (
 
 var collectInterval = 5 * time.Second
 
-var xprvStr = ""
+var xprvStr = "d20e3d81ba2c5509619fbc276d7cd8b94f52a1dce1291ae9e6b28d4a48ee67d8ac5826ba65c9da0b035845b7cb379e816c529194c7e369492d8828dee5ede3e2"
 
 func string2xprv(str string) (xprv chainkd.XPrv) {
 	if err := xprv.UnmarshalText([]byte(str)); err != nil {
