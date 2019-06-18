8fce6aec7686ce39019e3e9d4447fc60bbf3226d
HAOYUatHZ <haoyu@protonmail.com>
2019-06-18 10:46:44 +0800

    combine

--- federation/database/asset_keeper.go
+++ federation/database/asset_keeper.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"github.com/golang/groupcache/lru"
 	"github.com/jinzhu/gorm"
 
 	"github.com/vapor/errors"
@@ -50,4 +51,30 @@ func (a *AssetKeeper) Add(asset *orm.Asset) error {
 
 	a.assetCache.Add(asset.AssetID, asset)
 	return nil
+}
+
+const maxAssetCached = 1024
+
+type AssetCache struct {
+	lruCache *lru.Cache
+}
+
+func NewAssetCache() *AssetCache {
+	return &AssetCache{lruCache: lru.New(maxAssetCached)}
+}
+
+func (a *AssetCache) Add(assetID string, asset *orm.Asset) {
+	a.lruCache.Add(assetID, asset)
+}
+
+func (a *AssetCache) Get(assetID string) *orm.Asset {
+	if v, ok := a.lruCache.Get(assetID); ok {
+		return v.(*orm.Asset)
+	}
+
+	return nil
+}
+
+func (a *AssetCache) Remove(assetID string) {
+	a.lruCache.Remove(assetID)
 }
