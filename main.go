ba10498cc8694ac17b76905a57fdaf8aec08f64f
HAOYUatHZ <haoyu@protonmail.com>
2019-06-21 16:00:03 +0800

    wtf

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+
+}
+
+// # DONE
+// + listen & build
+//     + fix witnesss
+//     + sign and submit by hand
+//     + pass test
+// + refactor asset
+// + sign by code
+
+// # TODO
+// + refactor string
+// + request sign
+// + maybe witness
+// + xprv
+
+// + election
+// + withdrawal
