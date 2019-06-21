89067a56631a6c6bcf8eac2e58cccb12b521b9f3
HAOYUatHZ <haoyu@protonmail.com>
2019-06-21 16:48:31 +0800

    good

--- main.go
+++ main.go
@@ -1,13 +1,45 @@
 package main
 
 import (
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 )
 
 func main() {
-	bs := []byte{0}
-	inner(bs)
-	fmt.Println(bs)
+	// bs := []byte{0}
+	// inner(bs)
+	// fmt.Println(bs)
+
+	// var
+	raw := [][]byte{[]byte{1, 3}, []byte{2}}
+	// // fmt.Println(raw)
+
+	// str := "["
+	// str += "]"
+
+	var store []string
+	for _, part := range raw {
+		store = append(store, hex.EncodeToString(part))
+	}
+
+	b, _ /*err*/ := json.Marshal(store)
+	fmt.Println(string(b))
+	// fmt.Println(b)
+
+	var store2 []string
+	json.Unmarshal(b, &store2)
+	fmt.Println(store2)
+
+	var raw2 [][]byte
+	for _, part := range store2 {
+		b, _ := hex.DecodeString(part)
+		raw2 = append(raw2, b)
+	}
+
+	fmt.Println(raw)
+	fmt.Println(raw2)
+
 }
 
 func inner(bs []byte) {
