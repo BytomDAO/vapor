8ac1b4af6d2dcf61cc61650cb1e5e9803296e135
HAOYUatHZ <haoyu@protonmail.com>
2019-06-24 18:56:15 +0800

    clean

--- cmd/fedd/main.go
+++ cmd/fedd/main.go
@@ -8,11 +8,9 @@ import (
 	"github.com/vapor/federation/api"
 	"github.com/vapor/federation/config"
 	"github.com/vapor/federation/database"
-	// "github.com/vapor/federation/synchron"
+	"github.com/vapor/federation/synchron"
 )
 
-// TODO: should we rename bc package
-// https://github.com/golang/protobuf/issues/172
 func main() {
 	cfg := config.NewConfig()
 	db, err := database.NewMySQLDB(cfg.MySQLConfig)
@@ -20,9 +18,9 @@ func main() {
 		log.WithField("err", err).Panic("initialize mysql db error")
 	}
 
-	// assetStore := database.NewAssetStore(db)
-	// go synchron.NewMainchainKeeper(db, assetStore, cfg).Run()
-	// go synchron.NewSidechainKeeper(db, assetStore, cfg).Run()
+	assetStore := database.NewAssetStore(db)
+	go synchron.NewMainchainKeeper(db, assetStore, cfg).Run()
+	go synchron.NewSidechainKeeper(db, assetStore, cfg).Run()
 	go api.NewServer(db, cfg).Run()
 
 	// keep the main func running in case of terminating goroutines
