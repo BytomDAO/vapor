9fb9570e353f427a300b11a5bc4fc964518306bc
HAOYUatHZ <haoyu@protonmail.com>
2019-06-25 17:06:26 +0800

    fix https://github.com/Bytom/vapor/pull/181#discussion_r297058765

--- federation/api/handler.go
+++ federation/api/handler.go
@@ -22,13 +22,19 @@ func (s *Server) ListCrosschainTxs(c *gin.Context, listTxsReq *listCrosschainTxs
 	if listCompleted, err := listTxsReq.GetFilterBoolean("list_completed"); err == nil && listCompleted {
 		txFilter.Status = common.CrossTxCompletedStatus
 	}
+	if onlyFromMainchain, err := listTxsReq.GetFilterBoolean("only_from_mainchain"); err == nil && onlyFromMainchain {
+		txFilter.Chain.Name = common.MainchainName
+	}
+	if onlyFromSidechain, err := listTxsReq.GetFilterBoolean("only_from_sidechain"); err == nil && onlyFromSidechain {
+		txFilter.Chain.Name = common.SidechainName
+	}
 	if txHash, err := listTxsReq.GetFilterString("source_tx_hash"); err == nil && txHash != "" {
 		txFilter.SourceTxHash = txHash
 	}
 	if txHash, err := listTxsReq.GetFilterString("dest_tx_hash"); err == nil && txHash != "" {
 		txFilter.DestTxHash = sql.NullString{txHash, true}
 	}
-	txQuery := s.db.Where(txFilter).Preload("Reqs")
+	txQuery := s.db.Preload("Chain").Preload("Reqs").Where(txFilter)
 	txQuery = txQuery.Order(fmt.Sprintf("cross_transactions.source_block_height %s", listTxsReq.Sorter.Order))
 	txQuery = txQuery.Order(fmt.Sprintf("cross_transactions.source_tx_index %s", listTxsReq.Sorter.Order))
 	if err := txQuery.Offset(query.Start).Limit(query.Limit).Find(&ormTxs).Error; err != nil {
--- federation/common/const.go
+++ federation/common/const.go
@@ -1,6 +1,11 @@
 package common
 
 const (
+	MainchainName = "bytom"
+	SidechainName = "vapor"
+)
+
+const (
 	_ uint8 = iota
 	CrossTxPendingStatus
 	CrossTxCompletedStatus
