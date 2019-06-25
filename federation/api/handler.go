393ff2334ee6845f820336e40e7e55b952561608
HAOYUatHZ <haoyu@protonmail.com>
2019-06-25 11:59:32 +0800

    add

--- federation/api/handler.go
+++ federation/api/handler.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/vapor/errors"
@@ -10,12 +12,12 @@ import (
 
 func (s *Server) ListCrosschainTxs(c *gin.Context, listTxsReq *listCrosschainTxsReq, query *PaginationQuery) ([]*orm.CrossTransaction, error) {
 	var ormTxs []*orm.CrossTransaction
-
-	txQuery := s.db.Preload("Reqs")
+	txFilter := &orm.CrossTransaction{}
+	txQuery := s.db.Where(txFilter).Preload("Reqs")
 	if listPending, err := listTxsReq.GetFilterBoolean("list_pending"); err == nil && listPending {
 		txQuery = txQuery.Where("status = ?", common.CrossTxPendingStatus)
 	}
-	if listCompleted, err := listTxsReq.GetFilterBoolean("list_pending"); err == nil && listCompleted {
+	if listCompleted, err := listTxsReq.GetFilterBoolean("list_completed"); err == nil && listCompleted {
 		txQuery = txQuery.Where("status = ?", common.CrossTxCompletedStatus)
 	}
 	if txHash, err := listTxsReq.GetFilterString("source_tx_hash"); err == nil && txHash != "" {
@@ -24,7 +26,8 @@ func (s *Server) ListCrosschainTxs(c *gin.Context, listTxsReq *listCrosschainTxs
 	if txHash, err := listTxsReq.GetFilterString("dest_tx_hash"); err == nil && txHash != "" {
 		txQuery = txQuery.Where("dest_tx_hash = ?", txHash)
 	}
-	// txQuery = txQuery.Order(fmt.Sprintf("address_transactions.transaction_id %s", listTxsReq.Sorter.Order))
+	txQuery = txQuery.Order(fmt.Sprintf("cross_transactions.source_block_height %s", listTxsReq.Sorter.Order))
+	txQuery = txQuery.Order(fmt.Sprintf("cross_transactions.source_tx_index %s", listTxsReq.Sorter.Order))
 	if err := txQuery.Offset(query.Start).Limit(query.Limit).Find(&ormTxs).Error; err != nil {
 		return nil, errors.Wrap(err, "query txs")
 	}
