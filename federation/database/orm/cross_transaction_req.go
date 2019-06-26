f05c2421db923b2881f53ff80091a7bd4be55269
HAOYUatHZ <haoyu@protonmail.com>
2019-06-26 16:14:54 +0800

    add asset

--- federation/database/orm/cross_transaction_req.go
+++ federation/database/orm/cross_transaction_req.go
@@ -1,19 +1,33 @@
 package orm
 
 import (
+	"encoding/json"
+
 	"github.com/vapor/federation/types"
 )
 
 type CrossTransactionReq struct {
-	ID                 uint64          `gorm:"primary_key" json:"-"`
-	CrossTransactionID uint64          `json:"-"`
-	SourcePos          uint64          `json:"-"`
-	AssetID            uint64          `json:"-"`
-	AssetAmount        uint64          `json:"amount"`
-	Script             string          `json:"-"`
-	CreatedAt          types.Timestamp `json:"-"`
-	UpdatedAt          types.Timestamp `json:"-"`
+	ID                 uint64 `gorm:"primary_key"`
+	CrossTransactionID uint64
+	SourcePos          uint64
+	AssetID            uint64
+	AssetAmount        uint64
+	Script             string
+	CreatedAt          types.Timestamp
+	UpdatedAt          types.Timestamp
 
-	CrossTransaction *CrossTransaction `gorm:"foreignkey:CrossTransactionID" json:"-"`
-	Asset            *Asset            `gorm:"foreignkey:ID" json:"asset"`
+	CrossTransaction *CrossTransaction `gorm:"foreignkey:CrossTransactionID"`
+	Asset            *Asset            `gorm:"foreignkey:ID"`
+}
+
+func (c *CrossTransactionReq) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		AssetID   string `json:"asset_id"`
+		Amount    uint64 `json:"amount"`
+		ToAddress string `json:"to_address"`
+	}{
+		Amount:    c.AssetAmount,
+		ToAddress: ",",
+		AssetID:   c.Asset.AssetID,
+	})
 }
