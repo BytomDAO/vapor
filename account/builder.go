a2e9c9a7f34831fc378861dbde318ea81716907f
Chengcheng Zhang <943420582@qq.com>
2019-06-27 17:42:12 +0800

    update

--- account/builder.go
+++ account/builder.go
@@ -63,8 +63,8 @@ func MergeSpendAction(actions []txbuilder.Action) []txbuilder.Action {
 	return resultActions
 }
 
-//CalcMergeGas calculate the gas required that n utxos are merged into one
-func CalcMergeGas(num int) uint64 {
+//calcMergeGas calculate the gas required that n utxos are merged into one
+func calcMergeGas(num int) uint64 {
 	gas := uint64(0)
 	for num > 1 {
 		gas += chainTxMergeGas
@@ -76,7 +76,7 @@ func CalcMergeGas(num int) uint64 {
 func (m *Manager) ReserveBtmUtxoChain(builder *txbuilder.TemplateBuilder, accountID string, amount uint64, useUnconfirmed bool) ([]*UTXO, error) {
 	reservedAmount := uint64(0)
 	utxos := []*UTXO{}
-	for gasAmount := uint64(0); reservedAmount < gasAmount+amount; gasAmount = CalcMergeGas(len(utxos)) {
+	for gasAmount := uint64(0); reservedAmount < gasAmount+amount; gasAmount = calcMergeGas(len(utxos)) {
 		reserveAmount := amount + gasAmount - reservedAmount
 		res, err := m.utxoKeeper.Reserve(accountID, consensus.BTMAssetID, reserveAmount, useUnconfirmed, nil, builder.MaxTime())
 		if err != nil {
