7bee96f58ec891def394a52e4bae5213c55f8db1
HAOYUatHZ <haoyu@protonmail.com>
2019-07-02 20:13:42 +0800

    ??>

--- federation/synchron/porter.go
+++ federation/synchron/porter.go
@@ -1,9 +1,22 @@
 package synchron
 
-type Porter struct{}
+import (
+	"reflect"
+	"runtime"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Porter struct {
+	MainFuncs [](func() error)
+}
 
 func NewPorter() *Porter {
 	return &Porter{}
 }
 
-func (p *Porter) Run() {}
+func (p *Porter) Run() {
+	for _, f := range p.MainFuncs {
+		log.Info(runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name())
+	}
+}
