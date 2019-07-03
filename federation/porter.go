7537f1ee487094d2fde97f11d85129c54b984de6
HAOYUatHZ <haoyu@protonmail.com>
2019-07-03 14:56:52 +0800

    clean up

--- federation/porter.go
+++ federation/porter.go
@@ -8,21 +8,21 @@ import (
 )
 
 type Porter struct {
-	Callbacks [](func(p *Porter) error)
+	Callbacks [](func() error)
 }
 
 func NewPorter() *Porter {
 	return &Porter{}
 }
 
-func (p *Porter) AttachCallback(f func(p *Porter) error) {
+func (p *Porter) AttachCallback(f func() error) {
 	p.Callbacks = append(p.Callbacks, f)
 }
 
 func (p *Porter) Run() {
 	for _, f := range p.Callbacks {
 		log.Info("Running func: ", runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name())
-		f(p)
+		f()
 		log.Info("Func done: ", runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name())
 	}
 }
