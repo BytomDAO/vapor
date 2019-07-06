92f998a924b6c1a81baf0db651d2e93f49442c63
oys <oys@oysdeMBP.localdomain>
2019-07-06 11:59:44 +0800

    optimise

--- protocol/state/vote_result.go
+++ protocol/state/vote_result.go
@@ -44,7 +44,7 @@ func CalcVoteSeq(blockHeight uint64) uint64 {
 // ConsensusResult represents a snapshot of each round of DPOS voting
 // Seq indicates the sequence of current votes, which start from zero
 // NumOfVote indicates the number of votes each consensus node receives, the key of map represent public key
-// Finalized indicates whether this vote is finalized
+// CoinbaseReward indicates the coinbase receiver and reward
 type ConsensusResult struct {
 	Seq            uint64
 	NumOfVote      map[string]uint64
