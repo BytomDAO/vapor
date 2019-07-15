1a4040c2ff7fe5f94cd41184356cab4937eece19
mars <mars@bytom.io>
2019-07-15 16:00:33 +0800

    fix

--- toolbar/reward/database/dump_reward.sql
+++ toolbar/reward/database/dump_reward.sql
@@ -1,4 +1,4 @@
-SET NAMES utf8;
+SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS `block_state`;
 CREATE TABLE `block_state`  (
   `height` int(11) NOT NULL,
   `block_hash` varchar(64) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for vote
@@ -24,6 +24,6 @@ CREATE TABLE `vote`  (
   `output_id` varchar(64) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `xpub`(`xpub`, `vote_height`, `output_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
