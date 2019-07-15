e4280f132f677017d0f244fbeafa20e36bc12457
Wei Wang <apollo.mobility@gmail.com>
2019-07-15 17:02:17 +0800

    refactor

--- protocol/tx.go
+++ protocol/tx.go
@@ -33,27 +33,7 @@ func (c *Chain) ValidateTx(tx *types.Tx) (bool, error) {
 
 	c.markTransactions(tx)
 
-	if ok := c.txPool.HaveTransaction(&tx.ID); ok {
-		return false, c.txPool.GetErrCache(&tx.ID)
-	}
-
-	if c.txPool.IsDust(tx) {
-		c.txPool.AddErrCache(&tx.ID, ErrDustTx)
-		return false, ErrDustTx
-	}
-
-	bh := c.BestBlockHeader()
-	gasStatus, err := validation.ValidateTx(tx.Tx, types.MapBlock(&types.Block{BlockHeader: *bh}))
-	if !gasStatus.GasValid {
-		c.txPool.AddErrCache(&tx.ID, err)
-		return false, err
-	}
-
-	if err != nil {
-		log.WithFields(log.Fields{"module": logModule, "tx_id": tx.Tx.ID.String(), "error": err}).Info("transaction status fail")
-	}
-
-	return c.txPool.ProcessTransaction(tx, err != nil, bh.Height, gasStatus.BTMValue)
+	return c.validateTx(tx)
 }
 
 // validateTx validates the given transaction without checking duplication.
