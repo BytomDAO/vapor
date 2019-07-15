099c10465855f0f0d4ccefa8d4d3b7e4d42c198d
mars <mars@bytom.io>
2019-07-15 16:43:44 +0800

    modify CHARSET

--- toolbar/reward/database/dump_reward.sql
+++ toolbar/reward/database/dump_reward.sql
@@ -7,7 +7,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `block_state`;
 CREATE TABLE `block_state`  (
   `height` int(11) NOT NULL,
-  `block_hash` varchar(64) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+  `block_hash` varchar(64) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -16,12 +16,12 @@ CREATE TABLE `block_state`  (
 DROP TABLE IF EXISTS `vote`;
 CREATE TABLE `vote`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `xpub` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `voter_address` varchar(62) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `xpub` varchar(128) NOT NULL,
+  `voter_address` varchar(62) NOT NULL,
   `vote_height` int(11) NOT NULL,
   `vote_num` int(11) NOT NULL,
   `veto_height` int(11) NOT NULL,
-  `output_id` varchar(64) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `output_id` varchar(64) CHARACTER SET NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `xpub`(`xpub`, `vote_height`, `output_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARSET=utf8;
