8b08717cb7e7e9aa6fe8beb486e77999eb1b31ff
Chengcheng Zhang <943420582@qq.com>
2019-07-16 15:30:02 +0800

    update TestDeleteAccount

--- database/account_store_test.go
+++ database/account_store_test.go
@@ -106,33 +106,75 @@ func TestDeleteAccount(t *testing.T) {
 		},
 	}
 
-	accountStore := NewAccountStore(testDB)
 	for i, c := range cases {
+		accountStore := NewAccountStore(testDB)
+		as := accountStore.InitBatch()
 		// store mock accounts
 		for _, a := range c.accounts {
-			if err := accountStore.SetAccount(a); err != nil {
+			if err := as.SetAccount(a); err != nil {
 				t.Fatal(err)
 			}
 		}
 
 		// delete account
-		if err := accountStore.DeleteAccount(c.deleteAccount); err != nil {
+		if err := as.DeleteAccount(c.deleteAccount); err != nil {
+			t.Fatal(err)
+		}
+
+		if err := as.CommitBatch(); err != nil {
 			t.Fatal(err)
 		}
 
 		// get account by deleteAccount.ID, it should print ErrFindAccount
-		if _, err := accountStore.GetAccountByID(c.deleteAccount.ID); err != acc.ErrFindAccount {
+		if _, err := as.GetAccountByID(c.deleteAccount.ID); err != acc.ErrFindAccount {
 			t.Fatal(err)
 		}
 
 		for _, a := range c.want {
-			if _, err := accountStore.GetAccountByID(a.ID); err == acc.ErrFindAccount {
+			if _, err := as.GetAccountByID(a.ID); err == acc.ErrFindAccount {
 				t.Errorf("case %v: cann't find account, err: %v", i, err)
 			}
 
-			if _, err := accountStore.GetAccountByAlias(a.Alias); err == acc.ErrFindAccount {
+			if _, err := as.GetAccountByAlias(a.Alias); err == acc.ErrFindAccount {
 				t.Errorf("case %v: cann't find account, err: %v", i, err)
 			}
 		}
 	}
 }
+
+// func TestDeleteStandardUTXO(t *testing.T) {
+// 	testDB := dbm.NewDB("testdb", "leveldb", "temp")
+// 	defer func() {
+// 		testDB.Close()
+// 		os.RemoveAll("temp")
+// 	}()
+
+// 	cases := []struct {
+// 		utxos      []*acc.UTXO
+// 		deleteUTXO *acc.UTXO
+// 		want       []*acc.UTXO
+// 	}{
+// 		{
+// 			utxos:      []*acc.UTXO{},
+// 			deleteUTXO: &acc.UTXO{},
+// 			want:       []*acc.UTXO{},
+// 		},
+// 		{
+// 			utxos: []*acc.UTXO{
+// 				&acc.UTXO{
+// 					OutputID: bc.NewHash([32]byte{0x3e, 0x94, 0x5d, 0x35, 0x70, 0x30, 0xd4, 0x3b, 0x3d, 0xe3, 0xdd, 0x80, 0x67, 0x29, 0x9a, 0x5e, 0x09, 0xf9, 0xfb, 0x2b, 0xad, 0x5f, 0x92, 0xc8, 0x69, 0xd1, 0x42, 0x39, 0x74, 0x9a, 0xd1, 0x1c}),
+// 				},
+// 			},
+// 			deleteUTXO: &acc.UTXO{
+// 				OutputID: bc.NewHash([32]byte{0x3e, 0x94, 0x5d, 0x35, 0x70, 0x30, 0xd4, 0x3b, 0x3d, 0xe3, 0xdd, 0x80, 0x67, 0x29, 0x9a, 0x5e, 0x09, 0xf9, 0xfb, 0x2b, 0xad, 0x5f, 0x92, 0xc8, 0x69, 0xd1, 0x42, 0x39, 0x74, 0x9a, 0xd1, 0x1c}),
+// 			},
+// 			want: []*acc.UTXO{},
+// 		},
+// 	}
+
+// 	accountStore := NewAccountStore(testDB)
+
+// 	for i, c := range cases {
+// 		accountStore.
+// 	}
+// }
