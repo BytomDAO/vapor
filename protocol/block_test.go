040fba85825297cf674e5727114dfa951b11cd63
oys <oys@oysdeMBP.localdomain>
2019-07-19 13:34:28 +0800

    delete

--- protocol/block_test.go
+++ protocol/block_test.go
@@ -23,7 +23,7 @@ func (s *mStore) GetStoreStatus() *BlockStoreState                             {
 func (s *mStore) GetTransactionStatus(*bc.Hash) (*bc.TransactionStatus, error) { return nil, nil }
 func (s *mStore) GetTransactionsUtxo(*state.UtxoViewpoint, []*bc.Tx) error     { return nil }
 func (s *mStore) GetUtxo(*bc.Hash) (*storage.UtxoEntry, error)                 { return nil, nil }
-func (s *mStore) GetConsensusResult(uint64) (*state.ConsensusResult, error)    { return nil, nil }
+func (s *mStore) GetConsensusResult(uint64) (*state.ConsensusResult, error)              { return nil, nil }
 func (s *mStore) GetMainChainHash(uint64) (*bc.Hash, error)                    { return nil, nil }
 func (s *mStore) GetBlockHashesByHeight(uint64) ([]*bc.Hash, error)            { return nil, nil }
 func (s *mStore) SaveBlock(*types.Block, *bc.TransactionStatus) error          { return nil }
@@ -94,73 +94,3 @@ func TestCalcReorganizeChain(t *testing.T) {
 		t.Errorf("detachBlockHeaders is empty test: detach headers want %v but get %v", wantDetachBlockHeaders, getDetachBlockHeaders)
 	}
 }
-
-/*
-func TestMockReorganizeChain(t *testing.T) {
-	detachBlocks := []*types.Block{
-		&types.Block{
-			BlockHeader: types.BlockHeader{
-				Height:            consensus.MainNetParams.RoundVoteBlockNums + 1,
-				PreviousBlockHash: testutil.MustDecodeHash("4d08d14eea57211b41b5596828dbe9f34ca8ef40713042c57c6a3003b7147269"),
-			},
-			Transactions: []*types.Tx{
-				&types.Tx{
-					TxData: types.TxData{
-						Inputs:  []*types.TxInput{types.NewCoinbaseInput([]byte{0x03})},
-						Outputs: []*types.TxOutput{types.NewIntraChainOutput(bc.AssetID{}, 0, []byte{0x51})},
-					},
-				},
-			},
-		},
-		&types.Block{
-			BlockHeader: types.BlockHeader{
-				Height:            consensus.MainNetParams.RoundVoteBlockNums,
-				PreviousBlockHash: testutil.MustDecodeHash("4bcf2eed7829eab8220cee9e2e920c656aacb2de71619bad0536f6f7b9b00c2d"),
-			},
-			Transactions: []*types.Tx{
-				&types.Tx{
-					TxData: types.TxData{
-						Inputs: []*types.TxInput{types.NewCoinbaseInput([]byte{0x02})},
-						Outputs: []*types.TxOutput{
-							types.NewIntraChainOutput(bc.AssetID{}, 0, []byte{0x51}),
-							types.NewIntraChainOutput(bc.AssetID{}, consensus.BlockSubsidy(consensus.MainNetParams.RoundVoteBlockNums)+10000000, []byte{0x51}),
-							types.NewIntraChainOutput(bc.AssetID{}, 30000000, []byte{0x53}),
-							types.NewIntraChainOutput(bc.AssetID{}, 20000000, []byte{0x52}),
-						},
-					},
-				},
-			},
-		},
-		&types.Block{
-			BlockHeader: types.BlockHeader{
-				Height:            consensus.MainNetParams.RoundVoteBlockNums - 1,
-				PreviousBlockHash: testutil.MustDecodeHash("48f81bf65aeb0b3bf8d65c82d46149fd835a62a7b23b7a781781808066d6c1e3"),
-			},
-			Transactions: []*types.Tx{
-				&types.Tx{
-					TxData: types.TxData{
-						Inputs:  []*types.TxInput{types.NewCoinbaseInput([]byte{0x01})},
-						Outputs: []*types.TxOutput{types.NewIntraChainOutput(bc.AssetID{}, 0, []byte{0x51})},
-					},
-				},
-			},
-		},
-	}
-
-	consensusResult := state.ConsensusResult{
-		NumOfVote: map[string]uint64{},
-		CoinbaseReward: map[string]uint64{
-			"51": consensus.BlockSubsidy(consensus.MainNetParams.RoundVoteBlockNums),
-		},
-		BlockHash:   testutil.MustDecodeHash("391bffc3586e98067895bca12ffdbc82dff0fee1344e884b769727c1ac056ef7"),
-		BlockHeight: consensus.MainNetParams.RoundVoteBlockNums + 2,
-	}
-
-	for _, detachBlock := range detachBlocks {
-		if err := consensusResult.DetachBlock(detachBlock); err != nil {
-			t.Fatal(err)
-		}
-	}
-
-}
-*/
