1500a9b3db9d883b0f1a80200077165ca831bb59
mars <mars@bytom.io>
2019-07-22 14:40:01 +0800

    delete code

--- cmd/reward/main.go
+++ cmd/reward/main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"github.com/tendermint/tmlibs/cli"
-
-	"github.com/vapor/cmd/reward/command"
-)
-
 func main() {
-	cmd := cli.PrepareBaseCmd(command.RootCmd, "TM", "./")
-	cmd.Execute()
+
 }
