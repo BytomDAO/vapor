ab5fdab2cffa761437fb6a315cc6d14e5feb2eaa
iczc <iczcalan@gmail.com>
2019-07-24 17:22:05 +0800

    pick standbyBP vote result

---
+++ toolbar/consensusreward/consensus_reward.go
@@ -0,0 +1,38 @@
+package consensusreward
+
+import (
+	"fmt"
+
+	"github.com/vapor/api"
+	"github.com/vapor/consensus"
+)
+
+func PickStandbyBPVoteResult(voteResult []api.VoteInfo) ([]api.VoteInfo, error) {
+	low := int(consensus.ActiveNetParams.NumOfConsensusNode)
+	high := len(voteResult) - 1
+	position := 0
+	minConsensusNodeVoteNum := consensus.ActiveNetParams.MinConsensusNodeVoteNum
+	if high < 0 {
+		return nil, fmt.Errorf("Vote result is empty")
+	}
+	if high < 10 || voteResult[low].VoteNum < minConsensusNodeVoteNum {
+		return nil, fmt.Errorf("No Standby BP Node")
+	}
+	for low < high {
+		mid := int((low + high) / 2)
+		if voteResult[mid].VoteNum >= minConsensusNodeVoteNum {
+			low = mid + 1
+			if voteResult[low].VoteNum < minConsensusNodeVoteNum {
+				position = low
+				break
+			}
+		} else {
+			high = mid - 1
+			if voteResult[high].VoteNum >= minConsensusNodeVoteNum {
+				position = mid
+				break
+			}
+		}
+	}
+	return voteResult[int(consensus.ActiveNetParams.NumOfConsensusNode):position], nil
+}
