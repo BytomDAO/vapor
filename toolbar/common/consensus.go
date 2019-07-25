6138f556c88f9383047df089d600e7d71fb9648a
iczc <iczcalan@gmail.com>
2019-07-25 14:16:20 +0800

    use for loop

---
+++ toolbar/common/consensus.go
@@ -0,0 +1,25 @@
+package common
+
+import (
+	"sort"
+
+	"github.com/vapor/api"
+	"github.com/vapor/consensus"
+)
+
+const NumOfBPNode = 42
+
+func CalcStandByNodes(voteResult []api.VoteInfo) []api.VoteInfo {
+	sort.Slice(voteResult, func(i, j int) bool {
+		return voteResult[i].VoteNum > voteResult[j].VoteNum
+	})
+
+	result := []api.VoteInfo{}
+	for i := int(consensus.ActiveNetParams.NumOfConsensusNode); i < NumOfBPNode && i < len(voteResult); i++ {
+		if voteResult[i].VoteNum < consensus.ActiveNetParams.MinConsensusNodeVoteNum {
+			break
+		}
+		result = append(result, voteResult[i])
+	}
+	return result
+}
