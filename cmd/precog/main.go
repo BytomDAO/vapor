c51a1d8f1ad31c854162bbf384558e0961e070cf
HAOYUatHZ <haoyu@protonmail.com>
2019-08-02 11:18:42 +0800

    fix server run

--- cmd/precog/main.go
+++ cmd/precog/main.go
@@ -6,15 +6,18 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"github.com/vapor/toolbar/common"
+	"github.com/vapor/toolbar/precog/api"
 	"github.com/vapor/toolbar/precog/config"
 )
 
 func main() {
 	cfg := config.NewConfig()
-	_, err := common.NewMySQLDB(cfg.MySQLConfig)
+	db, err := common.NewMySQLDB(cfg.MySQLConfig)
 	if err != nil {
 		log.WithField("err", err).Panic("initialize mysql db error")
 	}
+
+	go api.NewApiServer(db).Run()
 
 	// keep the main func running in case of terminating goroutines
 	var wg sync.WaitGroup
--- toolbar/precog/api/server.go
+++ toolbar/precog/api/server.go
@@ -1,10 +1,15 @@
 package api
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 type server struct {
+	db *gorm.DB
 }
 
-func NewApiServer() {
-	return &server{}
+func NewApiServer(db *gorm.DB) *server {
+	return &server{db: db}
 }
 
 func (s *server) Run() {
