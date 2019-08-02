c5109f64fa4c73614f34389f2b31337845dc7a3b
HAOYUatHZ <haoyu@protonmail.com>
2019-08-02 16:51:46 +0800

    fix

--- toolbar/precog/database/orm/chain.go
+++ toolbar/precog/database/orm/chain.go
@@ -1,5 +1,11 @@
 package orm
 
+import (
+	"time"
+)
+
 type Chain struct {
 	BestHeight uint64
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 }
--- toolbar/precog/database/orm/node.go
+++ toolbar/precog/database/orm/node.go
@@ -1,9 +1,15 @@
 package orm
 
+import (
+	"time"
+)
+
 type Node struct {
 	Alias      string       `json:"alias"`
 	HostPort   string       `json:"host_port"`
 	PubKey     chainkd.XPub `json:"pubkey"`
 	BestHeight uint64       `json:"best_height"`
 	LantencyMS uint64       `json:"lantency_ms"` // TODO:
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 }
