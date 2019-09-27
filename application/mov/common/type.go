f7f97d18f9438c76a387909516f209a9c36931e2
wz <mars@bytom.io>
2019-09-27 14:21:40 +0800

    fix

--- application/mov/common/type.go
+++ application/mov/common/type.go
@@ -2,7 +2,7 @@ package common
 
 import "github.com/vapor/protocol/bc"
 
-type DexUtxo struct {
+type MovUtxo struct {
 	SourceID       *bc.Hash
 	SourcePos      uint64
 	Amount         uint64
@@ -12,7 +12,7 @@ type DexUtxo struct {
 type Order struct {
 	FromAssetID *bc.AssetID
 	ToAssetID   *bc.AssetID
-	Utxo        *DexUtxo
+	Utxo        *MovUtxo
 	Rate        float64
 }
 
@@ -22,7 +22,7 @@ type TradePair struct {
 	Count       uint64
 }
 
-type DexDatabaseState struct {
+type MovDatabaseState struct {
 	Height uint64
 	Hash   *bc.Hash
 }
