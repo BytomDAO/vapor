81754f391593eb25c6f16a5568a8484a7444f41b
wz <mars@bytom.io>
2019-10-23 19:48:54 +0800

    fix

--- common/crossin_asset.go
+++ common/crossin_asset.go
@@ -6,7 +6,9 @@ import (
 
 func IsCrossChainAssetOfNoBytom(rawDefinitionByte []byte) bool {
 	var defMap map[string]interface{}
-	json.Unmarshal(rawDefinitionByte, &defMap)
+	if err := json.Unmarshal(rawDefinitionByte, &defMap); err != nil {
+		return false
+	}
 
 	description, ok := defMap["description"].(map[string]interface{})
 	if !ok {
