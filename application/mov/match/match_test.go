d9aaad6b416a39fdf84dd4b392b654bc9d39abb4
shenao78 <shenao.78@163.com>
2019-10-25 09:43:58 +0800

    fix ci

--- application/mov/match/match_test.go
+++ application/mov/match/match_test.go
@@ -178,7 +178,7 @@ func TestGenerateMatchedTxs(t *testing.T) {
 
 	for i, c := range cases {
 		movStore := &database.MockMovStore{OrderMap: c.storeOrderMap}
-		matchEngine := NewEngine(movStore, []byte{0x51})
+		matchEngine := NewEngine(movStore, 0.05, []byte{0x51})
 		var gotMatchedTxs []*types.Tx
 		for matchEngine.HasMatchedTx(c.tradePair, c.tradePair.Reverse()) {
 			matchedTx, err := matchEngine.NextMatchedTx(c.tradePair, c.tradePair.Reverse())
