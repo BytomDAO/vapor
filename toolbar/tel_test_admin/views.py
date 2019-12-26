557fa944cf69029751b7c7c4fc24a3de6929fd52
Jay <976739120@qq.com>
2019-12-26 10:09:47 +0800

    Fix

--- toolbar/tel_test_admin/views.py
+++ toolbar/tel_test_admin/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify
 from functools import wraps
 from flask import request
 from uuid import uuid4
-
+import json
 tele = Blueprint('tele', __name__)
 
 # 全局变量
@@ -31,20 +31,20 @@ PASSWORD = "123456"
 def login_required(func):
     @wraps(func)
     def decorator(*args, **kwargs):
-        session_id = request.form.get('token')
-        if not session_id:
-            session_id = request.args.get('token')
+        data = json.loads(request.get_data(as_text=True))
+        session_id = data.get('token')
         if session_id not in LOGINED_UUID:
             return jsonify({"code": 400, "msg": "no user", "data": ""})
         else:
-            return func(*args, **kwargs)
+            return func(data)
 
     return decorator
 
 
 @tele.route('/login', methods=["POST"])
 def login():
-    password = request.form.get('password')
+    data = json.loads(request.get_data(as_text=True))
+    password = data["password"]
     if password != PASSWORD:
         return jsonify({"code": 400, "msg": "password error", "data": ""})
     session_id = uuid4()
@@ -64,14 +64,14 @@ def login():
 
 @tele.route('/get-all-node', methods=["GET","POST"])
 @login_required
-def get_all_node():
+def get_all_node(data):
     return jsonify({"code": 200, "msg": "", "data": NODE_LIST})
 
 
 @tele.route('/set-node-permission', methods=["POST"])
 @login_required
-def set_node_permission():
-    ip = request.form.get('ip')
+def set_node_permission(data):
+    ip = data.get('ip')
     level = request.form.get('level')
     for i in NODE_LIST:
         if i["IP"] == ip:
@@ -82,9 +82,9 @@ def set_node_permission():
 
 @tele.route('/set-connect', methods=["POST"])
 @login_required
-def set_connect():
-    ip = request.form.get('ip')
-    is_connect = request.form.get('is_connect')
+def set_connect(data):
+    ip = data.get('ip')
+    is_connect = data.get('is_connect')
     for i in NODE_LIST:
         if i["IP"] == ip:
             i["is_connect"] = is_connect
