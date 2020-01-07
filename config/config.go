f0bb080c5ace509aee2d91df36624968a2bfbbb7
shenao78 <shenao.78@163.com>
2020-01-07 09:53:12 +0800

    update seeds

--- config/config.go
+++ config/config.go
@@ -176,6 +176,7 @@ type P2PConfig struct {
 func DefaultP2PConfig() *P2PConfig {
 	return &P2PConfig{
 		ListenAddress:    "tcp://0.0.0.0:56656",
+		Seeds:            "52.82.122.197:56657",
 		SkipUPNP:         false,
 		LANDiscover:      true,
 		MaxNumPeers:      20,
@@ -248,10 +249,8 @@ func DefaultWebsocketConfig() *WebsocketConfig {
 func DefaultFederationConfig() *FederationConfig {
 	return &FederationConfig{
 		Xpubs: []chainkd.XPub{
+			xpub("2cd22b7d5fba12ed1e3324dd76ab2a3c5091a5a31c536e0a084ddbd803eadc2f2f531c63232118fa3874be94b7c7a17aed8e351ee63b287ff57f9bd3e53d1a74"),
 			xpub("580daf48fa8962100047cb1391da890bb7f2c849fdbc9b368cb4394a4c7cbb0977e2e7ebbf055dc0ef90af6a0d2af01ce7ec56b735d016aab597815ec48552e5"),
-			xpub("f3f6bcf61b65fa9d1566455a5688ca8b395efdc22e654963134b5e5cb0a45d8be522d21abc384a73177a7b9d64eba915fcfe2862d86a508a3c46dc410bdd72ad"),
-			xpub("53559612f2b7bcada18948b7de39d63947a0e2bd7336d07db1350c54ba5743996b84bf9d18ff7a2457e1a5c70ce5013e4a3b62666ddb03294c53051d5f5c70c0"),
-			xpub("7c88cc58adfc71818b08308d43c29de22460b0ea6895449cbec6e458d7dc09e0aea243fa5075ee6621da0d805bd047f6bb207329c5bd2ca3253b172fb323b512"),
 		},
 		Quorum: 2,
 	}
