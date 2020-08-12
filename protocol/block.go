71c2e483000203b370d7882a5a6fb730c946ff11
shenao78 <shenao.78@163.com>
2020-08-12 19:05:35 +0800

    fix rollback exception

--- protocol/block.go
+++ protocol/block.go
@@ -20,6 +20,7 @@ var (
 
 // BlockExist check is a block in chain or orphan
 func (c *Chain) BlockExist(hash *bc.Hash) bool {
+	return false
 	if _, err := c.store.GetBlockHeader(hash); err == nil {
 		return true
 	}
