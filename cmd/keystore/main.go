62715b59ff733837a076128ab6f36ed013104b18
ipqhjjybj <250657661@qq.com>
2020-09-22 17:58:42 +0800

    add code

--- cmd/keystore/main.go
+++ cmd/keystore/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"encoding/json"
+)
+
+type PersonInfo struct {
+	Name    string
+	age     int32
+	Sex     bool
+	Hobbies []string
+}
+
+func readFile() {
+
+	filePtr, err := os.Open("person_info.json")
+	if err != nil {
+		fmt.Println("Open file failed [Err:%s]", err.Error())
+		return
+	}
+	defer filePtr.Close()
+
+	var person []PersonInfo
+
+	// 创建json解码器
+	decoder := json.NewDecoder(filePtr)
+	err = decoder.Decode(&person)
+	if err != nil {
+		fmt.Println("Decoder failed", err.Error())
+
+	} else {
+		fmt.Println("Decoder success")
+		fmt.Println(person)
+	}
+}
 
 func main() {
 	fmt.Println("Hello world!")
+	readFile()
 }
