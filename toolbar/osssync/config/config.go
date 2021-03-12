b073bffde12f0cac9bc76e4874e19e993abdbcae
Welt <L5Accelerator@users.noreply.github.com>
2021-03-12 15:30:14 +0800

    Update config.go

--- toolbar/osssync/config/config.go
+++ toolbar/osssync/config/config.go
@@ -15,9 +15,9 @@ type Config struct {
 
 // Oss logs cfg
 type Oss struct {
-	Endpoint        string `json:"endpoint" mapstructure:"endpoint"`
-	AccessKeyID     string `json:"access_key_id" mapstructure:"access_key_id"`
-	AccessKeySecret string `json:"access_key_secret" mapstructure:"access_key_secret"`
+	Endpoint        string `json:"endpoint"`
+	AccessKeyID     string `json:"access_key_id"`
+	AccessKeySecret string `json:"access_key_secret"`
 }
 
 // LoadConfig read path file to the config object
