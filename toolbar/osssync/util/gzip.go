f1f375cdbb3c4f02aa222b84161c659114f8dd92
Outer-God <39217235+Outer-God@users.noreply.github.com>
2021-03-16 11:07:48 +0800

    util (#575)
    * util
    
    * Update gzip.go
    
    filePath fileName
    
    * Update gzip.go
    
    * Update json.go
    
    * error
    
    * Update json.go
    
    * Update json.go
    
    Co-authored-by: Welt <L5Accelerator@users.noreply.github.com>

---
+++ toolbar/osssync/util/gzip.go
@@ -0,0 +1,84 @@
+package util
+
+import (
+	"compress/gzip"
+	"os"
+)
+
+const READ_SIZE = 1024 * 1024 * 500
+
+// GzipCompress compress file to Gzip
+func (f *FileUtil) GzipCompress(fileName string) error {
+	filePath := f.localDir + "/" + fileName + ".json.gz"
+	fw, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+
+	defer fw.Close()
+
+	gw := gzip.NewWriter(fw)
+	defer gw.Close()
+
+	filePath = f.localDir + "/" + fileName + ".json"
+	fr, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+
+	defer fr.Close()
+
+	fi, err := fr.Stat()
+	if err != nil {
+		return err
+	}
+
+	gw.Header.Name = fi.Name()
+
+	buf := make([]byte, fi.Size())
+	_, err = fr.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	_, err = gw.Write(buf)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+// GzipUncompress uncompress Gzip file
+func (f *FileUtil) GzipUncompress(fileName string) error {
+	filedirname := f.localDir + "/" + fileName + ".json.gz"
+	fr, err := os.Open(filedirname)
+	if err != nil {
+		return err
+	}
+
+	defer fr.Close()
+
+	gr, err := gzip.NewReader(fr)
+	if err != nil {
+		return err
+	}
+
+	defer gr.Close()
+
+	buf := make([]byte, READ_SIZE)
+	n, err := gr.Read(buf)
+
+	filedirname = f.localDir + "/" + gr.Header.Name
+	fw, err := os.Create(filedirname)
+	if err != nil {
+		return err
+	}
+
+	_, err = fw.Write(buf[:n])
+	if err != nil {
+		return err
+	}
+
+	return err
+}
---
+++ toolbar/osssync/util/json.go
@@ -0,0 +1,77 @@
+package util
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+type FileUtil struct {
+	localDir string
+}
+
+// NewFileUtil creates new file util
+func NewFileUtil(localDir string) *FileUtil {
+	return &FileUtil{localDir}
+}
+
+// SaveBlockFile saves block file
+func (f *FileUtil) SaveBlockFile(filename string, data interface{}) (bool, error) {
+	filename = f.localDir + "/" + filename + ".json"
+	saveData, err := json.Marshal(data)
+	if err != nil {
+		return false, err
+	}
+
+	err = ioutil.WriteFile(filename, saveData, 0644)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// GetJson read json file
+func (f *FileUtil) GetJson(filename string) (json.RawMessage, error) {
+	filename = f.localDir + "/" + filename + ".json"
+	return ioutil.ReadFile(filename)
+}
+
+// RemoveLocal deletes file
+func (f *FileUtil) RemoveLocal(filename string) error {
+	return os.Remove(f.localDir + "/" + filename)
+}
+
+// Json2Struct transform json to struct
+func Json2Struct(data json.RawMessage, resp interface{}) error {
+	return json.Unmarshal(data, &resp)
+}
+
+// Struct2Json transform struct to json
+func Struct2Json(theStruct interface{}) (json.RawMessage, error) {
+	return json.Marshal(theStruct)
+}
+
+// IsExists if file or directory exist
+func IsExists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil && !os.IsExist(err) {
+		return false
+	}
+	return true
+}
+
+// IfNoFileToCreate if the file is not exist, create the file
+func IfNoFileToCreate(fileName string) (file *os.File) {
+	var f *os.File
+	var err error
+	if !IsExists(fileName) {
+		f, err = os.Create(fileName)
+		if err != nil {
+			return
+		}
+
+		defer f.Close()
+	}
+	return f
+}
