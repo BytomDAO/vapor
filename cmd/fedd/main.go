583523e5ccf71050dca47c205e72bd989501bc37
songxuexian <s.xue.xian.c@gmail.com>
2021-05-11 16:42:57 +0800

    split fed api and keeper

--- cmd/fedd/main.go
+++ cmd/fedd/main.go
@@ -19,9 +19,9 @@ func main() {
 		log.WithField("err", err).Panic("initialize mysql db error")
 	}
 
-	assetStore := database.NewAssetStore(db)
-	go synchron.NewMainchainKeeper(db, assetStore, cfg).Run()
-	go synchron.NewSidechainKeeper(db, assetStore, cfg).Run()
+	// assetStore := database.NewAssetStore(db)
+	// go synchron.NewMainchainKeeper(db, assetStore, cfg).Run()
+	// go synchron.NewSidechainKeeper(db, assetStore, cfg).Run()
 	go api.NewServer(db, cfg).Run()
 
 	// keep the main func running in case of terminating goroutines
