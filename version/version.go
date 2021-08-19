8c59982986e789ef6d35b44ad4598e4487326aff
yzb@example.cn <yzb@example.cn>
2021-08-19 10:35:31 +0800

    versoin1.1.9

--- version/version.go
+++ version/version.go
@@ -47,7 +47,7 @@ const (
 
 var (
 	// The full version string
-	Version = "1.1.8"
+	Version = "1.1.9"
 	// GitCommit is set with --ldflags "-X main.gitCommit=$(git rev-parse HEAD)"
 	GitCommit string
 	Status    *UpdateStatus
